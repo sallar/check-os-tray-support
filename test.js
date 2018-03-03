0323838fab2f49dec5875c5f1b27bcc68a68faa5
Sallar Kaboli <sallar.kaboli@gmail.com>
2018-03-03 22:12:33 +0200

    Update test

--- test.js
+++ test.js
@@ -1,13 +1,6 @@
-const check = require('./');
-
-// check().then(res => {
-//   console.log('done', res);
-// });
-
-console.log(check());
-
 import test from 'ava';
+import check from './';
 
-test('arrays are equal', t => {
-  t.deepEqual([1, 2], [1, 2]);
+test('libappindicator1 is', t => {
+  t.is(check(), true);
 });
